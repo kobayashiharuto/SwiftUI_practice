4538c087611ad212c2c583937205f5cf7ea11fe8
kobayashiharuto <datesaver1234@gmail.com>
2021-03-23 00:18:21 +0900

    Initial Commit

---
+++ SwiftUI_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_test
+//
+//  Created by 小林春斗 on 2021/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_test/SwiftUI_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_testApp.swift
+//  SwiftUI_test
+//
+//  Created by 小林春斗 on 2021/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
