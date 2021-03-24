587a853d9e25acb93bb8c1f73457a0add43858dd
kobayashiharuto <datesaver1234@gmail.com>
2021-03-25 02:35:59 +0900

    fin sec7

---
+++ SwiftUI_test/Views/LandmarkDetail.swift
@@ -0,0 +1,53 @@
+//
+//  LandmarkDetail.swift
+//  SwiftUI_test
+//
+//  Created by 小林春斗 on 2021/03/25.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+  var landmark: Landmark
+  
+  var body: some View {
+    ScrollView {
+      MapView(coordinate: landmark.locationCoordinate)
+        .ignoresSafeArea(edges: .top)
+        .frame(height: 300)
+      
+      CircleImage(image: landmark.image)
+        .offset(y: -130)
+        .padding(.bottom, -130.0)
+      
+      VStack(alignment: .leading) {
+        Text(landmark.name)
+          .font(.title)
+        
+        HStack {
+          Text(landmark.park)
+            .font(.subheadline)
+          Spacer()
+          Text(landmark.state)
+            .font(.subheadline)
+        }
+        
+        Divider()
+        
+        Text("About \(landmark.name)")
+          .font(.subheadline)
+        Text(landmark.description)
+          .font(.subheadline)
+      }
+      .padding()
+    }
+    .navigationTitle(landmark.name)
+    .navigationBarTitleDisplayMode(.inline)
+  }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+  static var previews: some View {
+    LandmarkDetail(landmark: landmarks[0])
+  }
+}
