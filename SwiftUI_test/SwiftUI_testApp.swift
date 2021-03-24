4f43a6a3aed1593ecd619a719b96a6d3a690bbdf
kobayashiharuto <datesaver1234@gmail.com>
2021-03-24 22:24:43 +0900

    first commit

--- SwiftUI_test/SwiftUI_testApp.swift
+++ SwiftUI_test/SwiftUI_testApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct SwiftUI_testApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
