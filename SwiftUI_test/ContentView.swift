8e2460a54a79faee0e75edb026ce2b92bbab252b
kobayashiharuto <datesaver1234@gmail.com>
2021-03-25 00:10:04 +0900

    fin section2

--- SwiftUI_test/ContentView.swift
+++ SwiftUI_test/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 struct ContentView: View {
   var body: some View {
     Text("Hello!")
-      .padding(0)
+      .font(.title)
+      .fontWeight(.bold)
+      .foregroundColor(Color.red)
+      .padding(4.0)
     
   }
 }
