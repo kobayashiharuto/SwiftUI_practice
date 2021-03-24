899a83f1e64b50545e423cdcecf581efd0469636
kobayashiharuto <datesaver1234@gmail.com>
2021-03-25 01:48:21 +0900

    fin sec2

---
+++ SwiftUI_test/Model/Landmark.swift
@@ -0,0 +1,35 @@
+//
+//  Landmark.swift
+//  SwiftUI_test
+//
+//  Created by 小林春斗 on 2021/03/25.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+  var id: Int
+  var name: String
+  var park: String
+  var state: String
+  var description: String
+  
+  private var imageName: String
+  var image: Image {
+    Image(imageName)
+  }
+  
+  private var coordinates: Coordinates
+  var locationCoordinate: CLLocationCoordinate2D {
+    CLLocationCoordinate2D(
+      latitude: coordinates.latitude,
+      longitude: coordinates.longitude)
+  }
+  
+  struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longitude: Double
+  }
+}
