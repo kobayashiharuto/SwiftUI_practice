ae1ba570e73f18984ab4d6635f51f7c47596b617
kobayashiharuto <datesaver1234@gmail.com>
2021-03-25 20:57:49 +0900

    ssh test

--- SwiftUI_test/Views/CategoryHome.swift
+++ SwiftUI_test/Views/CategoryHome.swift
@@ -12,6 +12,7 @@ struct CategoryHome: View {
       NavigationView {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
           .navigationTitle("Featured")
+        
       }
     }
 }
