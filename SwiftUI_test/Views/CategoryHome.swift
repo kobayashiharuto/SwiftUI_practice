fbbfd4a5451acbf4b3208afd7c8dd60d49584b59
kobayashiharuto <datesaver1234@gmail.com>
2021-03-25 20:38:53 +0900

    fin sec1

---
+++ SwiftUI_test/Views/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  SwiftUI_test
+//
+//  Created by 小林春斗 on 2021/03/25.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+      NavigationView {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+          .navigationTitle("Featured")
+      }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
