33ab61ec738e86ffbc28b22c0894324a133fba22
kobayashiharuto <datesaver1234@gmail.com>
2021-03-25 21:38:40 +0900

    fin sec2

---
+++ SwiftUI_test/Views/CategoryItem.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryItem.swift
+//  SwiftUI_test
+//
+//  Created by 小林春斗 on 2021/03/25.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItem()
+    }
+}
