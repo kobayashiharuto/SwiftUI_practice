e5fd00cb233e4517eebe93f5bbfb1a89723040f0
kobayashiharuto <datesaver1234@gmail.com>
2021-03-25 21:38:52 +0900

    fin sec3

--- SwiftUI_test/Views/CategoryRow.swift
+++ SwiftUI_test/Views/CategoryRow.swift
@@ -8,13 +8,35 @@
 import SwiftUI
 
 struct CategoryRow: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var categoryName: String
+  var items: [Landmark]
+  
+  var body: some View {
+    VStack(alignment: .leading) {
+      Text(categoryName)
+        .font(.headline)
+        .padding(.leading, 15)
+        .padding(.top, 5)
+      
+      ScrollView(.horizontal, showsIndicators: false) {
+        HStack(alignment: .top, spacing: 0) {
+          ForEach(items) { landmark in
+            CategoryItem(landmark: landmark)
+          }
+        }
+      }
+      .frame(height: 185)
     }
+  }
 }
 
 struct CategoryRow_Previews: PreviewProvider {
-    static var previews: some View {
-        CategoryRow()
-    }
+  static var landmarks = ModelData().landmarks
+  
+  static var previews: some View {
+    CategoryRow(
+      categoryName: landmarks[0].category.rawValue,
+      items: Array(landmarks.prefix(3))
+    )
+  }
 }
